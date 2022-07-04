e8350ab50fffe912a6176e112e99d358aa1bfb37
Szymon Wojaczek <swojaczek@oferteo.pl>
2022-07-04 12:25:51 +0200

    draw grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+const grid = [];
+
 function setup() {
     createCanvas(400, 400);
+
+    grid.push([0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]);
+
+    console.table(grid);
+    addNumber();
+    console.table(grid);
+}
+
+function addNumber() {
+    const options = [];
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] === 0) {
+                options.push({ x: i, y: j });
+            }
+        }
+    }
+
+    if (options.length > 0) {
+        const spot = random(options);
+        const randomNumber = random(1);
+
+        grid[spot.x][spot.y] = randomNumber > 0.5 ? 2 : 4;
+    }
 }
 
 function draw() {
-    background(0);
+    background(255);
+
+    const width = 100;
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            noFill();
+            strokeWeight(2);
+            stroke(0);
+            rect(width * i, width * j, width, width);
+        }
+    }
 }
